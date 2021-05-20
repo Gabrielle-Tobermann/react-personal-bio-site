8aee9cafd33b348c14abc8c9f0376461a2017d80
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-05-20 15:12:41 -0500

    trying to setup auth

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase';
+import 'firebase/auth';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebaseConfig from '../helpers/apiKeys';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/routes';
 import './App.scss';
 
-function App() {
-  // const [domWriting, setDomWriting] = useState('Nothing Here!');
+firebase.initializeApp(firebaseConfig);
 
-  // const handleClick = (e) => {
-  //   console.warn(`You clicked ${e.target.id}`);
-  //   setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  // };
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        setUser(userObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
 
   return (
     <div className='App'>
