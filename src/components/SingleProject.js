8b0ae963ce0cc5847eed1dd8c6d545d82bc48177
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-05-25 18:17:10 -0500

    single-project

--- src/components/SingleProject.js
+++ src/components/SingleProject.js
@@ -14,8 +14,8 @@ function SingleProject() {
 
   return (
     <div>
-      <h1>{`${projectObj.title}&output=embed`}</h1>
-      <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}><iframe src={projectObj.loom} frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen="true" style={{
+      <h1>{projectObj.title}</h1>
+      <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0 }}><iframe src={`${projectObj.loom}&output=embed`} frameBorder="0" webkitallowfullscreen="true" mozallowfullscreen="true" allowFullScreen="true" style={{
         position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'
       }}></iframe></div>
     </div>
