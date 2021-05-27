d92aa3cfd4ce7b93333795118ebf8599bb7cfe38
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-05-27 17:01:48 -0500

    email can be copied

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typist from 'react-typist';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Tooltip } from 'reactstrap';
 import {
   Info, InfoContainer, WholeContainer, IconContainer, Title
 } from '../styles/ContactStyle';
 
 export default function Contact() {
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const toggle = () => setTooltipOpen(!tooltipOpen);
   return (
     <div>
       <Typist cursor={{ show: false }}>
@@ -17,7 +22,12 @@ export default function Contact() {
           <i className="fab fa-github fa-5x"></i>
         </IconContainer>
         <InfoContainer>
-          <Info>gabrielle.tobermann@gmail.com</Info>
+          <span>
+            <CopyToClipboard text="gabrielle.tobermann@gmail.com">
+            <Info href="#" id="GabEmail">gabrielle.tobermann@gmail.com</Info>
+            </CopyToClipboard>
+          </span>
+          <Tooltip placement="top" isOpen={tooltipOpen} target="GabEmail" toggle={toggle}>Copy</Tooltip>
           <Info href="https://www.linkedin.com/in/gabrielle-tobermann-347878153/">https://www.linkedin.com/in/gabrielle-tobermann-347878153/</Info>
           <Info href="https://github.com/Gabrielle-Tobermann">https://github.com/Gabrielle-Tobermann</Info>
         </InfoContainer>
